2dcf5a17beab9dfb827e1c34f2226a01dda5982c
Jason Wright <noobjey@gmail.com>
2015-11-21 11:55:52 -0700

    start new list page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get "/dashboard", to: "dashboard#index"
+
+  resources :lists, only: [:new]
 end
--- spec/task_list_spec.rb
+++ spec/task_list_spec.rb
@@ -8,11 +8,11 @@ RSpec.feature "Lists:", type: :feature do
       visit dashboard_path
 
       within "#task-lists" do
-        expect page.to have_button "New Task"
-        click_on "New Task"
+        expect(page).to have_link "New List"
+        click_on "New List"
       end
 
-      expect(current_path).to eq(new_task_path)
+      expect(current_path).to eq(new_list_path)
     end
   end
 end
