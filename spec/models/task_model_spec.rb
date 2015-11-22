9f228a5e10de4a4954a2101b59feec209a9c9535
Jason Wright <noobjey@gmail.com>
2015-11-22 09:02:43 -0700

    add model tests for tasks

---
+++ spec/models/task_model_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe Task, type: :model do
+
+  describe "Task:" do
+
+    context "status scopes" do
+      let!(:completed_task) { Task.create(status: true) }
+      let!(:uncompleted_task) { Task.create(status: false) }
+
+      it "#uncompeleted" do
+        tasks = Task.uncompleted
+
+        expect(tasks.count).to eq(1)
+        expect(tasks.first).to eq(uncompleted_task)
+      end
+
+      it "returns text value of status" do
+        expect(completed_task.status_string).to eq("complete")
+        expect(uncompleted_task.status_string).to eq("incomplete")
+      end
+    end
+
+  end
+end
