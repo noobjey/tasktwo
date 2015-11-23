01ff9fe0783972d32aee7b8603fc38f271b99760
Jason Wright <noobjey@gmail.com>
2015-11-23 11:05:44 -0700

    add login tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,10 +8,8 @@ class SessionsController < ApplicationController
   def create
     if user = User.find_or_create_from_oauth(request.env['omniauth.auth'])
       session[:user_id] = user.id
-      redirect_to dashboard_path
-    else
-      redirect_to root_path
     end
+    redirect_to dashboard_path
   end
 
   def destroy
