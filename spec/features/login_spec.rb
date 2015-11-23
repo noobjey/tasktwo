f4060b95b5dc6bc2a35ad2438dbfdff2a2605058
Jason Wright <noobjey@gmail.com>
2015-11-22 20:03:22 -0700

    add logout and header

---
+++ spec/features/login_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+require 'support/login_helper'
+
+RSpec.feature "Logins:", type: :feature do
+
+  include LoginHelper
+
+  before do
+    stub_omniauth_github()
+  end
+
+  describe "a user that visits the login page" do
+    let(:login_button) { "Login with Github" }
+
+    before do
+      visit root_path
+    end
+
+    context "and is not logged in" do
+
+      it "sees a login button" do
+        expect(current_path).to eq root_path
+        expect(page).to have_link login_button
+      end
+
+      it "can login" do
+        click_on login_button
+
+        expect(current_path).to eq dashboard_path
+        expect(page).to have_content "Logout"
+      end
+    end
+
+
+    context "and has already logged in" do
+      before do
+        visit root_path
+        click_on login_button
+      end
+
+      it "sees their dashboard" do
+        visit root_path
+
+        expect(current_path).to eq(dashboard_path)
+      end
+    end
+  end
+end
