5d8398c4f4c2b2b75b453e4643edd3755bbc6dab
Jason Wright <noobjey@gmail.com>
2015-11-23 08:47:26 -0700

    due date validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ActiveRecord::Base
   belongs_to :list
 
+  validates :title, presence: true, allow_blank: false
+  validate :valid_due_date
+
   def self.uncompleted
     Task.where(status: false)
   end
@@ -17,4 +20,22 @@ class Task < ActiveRecord::Base
   def due_date_formatted
     self.due_date.strftime("%d %B %Y")
   end
+
+
+  private
+
+  def valid_due_date
+    if !due_date.is_a?(Date) && turn_into_date?(due_date)
+      errors.add(:due_date, "Must enter a valid date.")
+      false
+    else
+      true
+    end
+  end
+
+  def turn_into_date?(due_date)
+    ((Date.parse(due_date) rescue ArgumentError) == ArgumentError)
+  end
 end
+
+
--- spec/models/task_model_spec.rb
+++ spec/models/task_model_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Task, type: :model do
   describe "Task:" do
 
     context "status scopes" do
-      let!(:completed_task) { Task.create(status: true) }
-      let!(:uncompleted_task) { Task.create(status: false) }
+      let!(:completed_task) { Task.create(title: "title", status: true, due_date: Date.today()) }
+      let!(:uncompleted_task) { Task.create(title: "title", status: false, due_date: Date.today()) }
 
       it "#uncompeleted" do
         tasks = Task.uncompleted
@@ -25,6 +25,27 @@ RSpec.describe Task, type: :model do
       it "returns text value of status" do
         expect(completed_task.status_string).to eq("complete")
         expect(uncompleted_task.status_string).to eq("active")
+      end
+
+      context "validations" do
+        it "requires title" do
+          task = Task.new(title: "")
+
+          expect(task.valid?).to eq(false)
+        end
+
+        it "requires date" do
+          task = Task.new(title: "title")
+
+          expect(task.valid?).to eq(false)
+        end
+
+
+        it "date cannot be random string" do
+          task = Task.new(title: "title", due_date: "not a date")
+
+          expect(task.valid?).to eq(false)
+        end
       end
     end
 
